63a2189f888703a9b6457fb62390ddfb849b3aa7
Karolis_Germanavicius <karolis.germanavicius@ktu.edu>
2018-03-27 11:51:52 +0300

    fixed "take damage"

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -287,6 +287,12 @@ public class EnemyBehaviour : MonoBehaviour
             }
             transform.rotation = Quaternion.Euler(angles);
         }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                HealthBarScript.Damage();
+            }
+        }
 
-
-    }
+}
