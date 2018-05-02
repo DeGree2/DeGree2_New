9d3e172d1031b9d3433612e724c3cd0feca10de0
Milda Petrikaitė <milda.petrikaite@ktu.edu>
2018-05-02 03:52:32 +0300

    CHA9 - improved puzzles. Added readable object - a note or a document, which has an imaged attached to it and that image is shown on screen when interacted. Can be dismissed by clicking on that image. Also, added keypad, which is another way of locking doors. Needs a right combination to unlock doors. Using keypad brings up another canvas, which has buttons. Code can be acquired through puzzle solving (usually, in notes). Left note templates for future note making. Made some small scene changes.

---
+++ Assets/Scripts/ReadableObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadableObject : InteractionObject
+{
+    public GameObject panel; //panel which is shown on screen when object is interacted with
+    public Sprite noteImage; //image of "note" that will be shown on screen when reading
+
+    private void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    public override void DoInteraction()
+    {
+        panel.GetComponentInChildren<Image>().sprite = noteImage; //changes the image to note image
+        panel.SetActive(true);
+    }
+}
